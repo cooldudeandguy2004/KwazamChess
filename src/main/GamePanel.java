01ec072035df0351cfbf666321a642b878aff5d0
Ammar <ammarmuqriz6555@gmail.com>
2025-01-20 19:28:56 +0800

    Dah buat board and letak pieces tapi tak tahu cane nak letak pieces tu atas board Asyik error

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,13 +7,18 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
     public static final int WIDTH = 800;
-    public static final int HEIGHT = 800;
+    public static final int HEIGHT = 900;
     final int FPS = 60;
     Thread gameThread;
     Board board = new Board();
+
+    // Color
+    public static final int RED = 1;
+    public static final int BLUE = 0;
+    int currentColor = BLUE;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -27,8 +32,8 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        //Game loop
-        double drawInterval = 1000000000/FPS;
+        // Game loop
+        double drawInterval = 1000000000 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -39,13 +44,12 @@ public class GamePanel extends JPanel implements Runnable{
             lastTime = currentTime;
 
             if (delta >= 1) {
-            update();
-            repaint();
-            delta--;
+                update();
+                repaint();
+                delta--;
             }
         }
 
-        
     }
 
     private void update() {
@@ -55,10 +59,8 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         board.draw(g2);
     }
-
-    
 
 }
